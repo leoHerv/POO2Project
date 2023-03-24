76b2842f4ae680f99ff16b102fa0dbac7a490ea1
Léo Hervouet <108589080+leoHerv@users.noreply.github.com>
2023-03-24 09:05:59 +0100

    Make the IntelliJ set up and the basic directories with the controller of the game

---
+++ POO2Project/src/main/java/jeudesfourmis/controller/Controlleur.java
@@ -0,0 +1,25 @@
+package jeudesfourmis.controller;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Controlleur extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+        BorderPane base = new BorderPane();
+
+        Scene scene = new Scene(base, 160, 300);
+
+        primaryStage.setTitle("ApllicationJF");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ POO2Project/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module jeudesfourmis {
+    requires javafx.controls;
+
+    exports jeudesfourmis.controller;
+}
